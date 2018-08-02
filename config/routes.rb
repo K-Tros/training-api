24305dd18370bd429027c8104cfb2f02f13407b5
Trosinski <q1009652@usboswly15453.quintiles.net>
2018-08-02 13:20:22 -0400

    Remove some more unneeded code

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'configs/set'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'api/v1/timers', to: 'timers#post'
   post 'api/v1/configs', to: 'configs#set'
